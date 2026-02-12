e545a3d2f269cefd5f1823ae1eb8a6960a133158
JChorao <joao.97.oliveira@gmail.com>
2026-02-12 03:20:21 +0000

    Veriosn 1.3
    Instavel

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,1 @@
-"""Módulo de utilitários"""
 from .network import get_local_ip, generate_self_signed_cert, cleanup_app_data
-
-__all__ = ['get_local_ip', 'generate_self_signed_cert', 'cleanup_app_data']
--- utils/network.py
+++ utils/network.py
@@ -21,40 +21,34 @@ def generate_self_signed_cert(cert_file="server.crt", key_file="server.key"):
     if cert_path.exists() and key_path.exists():
         return str(cert_path), str(key_path)
     
-    try:
-        from OpenSSL import crypto
-        local_ip = get_local_ip()
-        key = crypto.PKey()
-        key.generate_key(crypto.TYPE_RSA, 2048)
-        cert = crypto.X509()
-        cert.get_subject().CN = local_ip
-        cert.set_serial_number(1000)
-        cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(365*24*60*60)
-        cert.set_issuer(cert.get_subject())
-        cert.set_pubkey(key)
-        cert.sign(key, 'sha256')
-        
-        with open(cert_path, "wb") as f:
-            f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
-        with open(key_path, "wb") as f:
-            f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
-            
-        return str(cert_path), str(key_path)
-    except ImportError:
-        os.system("pip install pyOpenSSL")
-        return generate_self_signed_cert(cert_file, key_file)
+    from OpenSSL import crypto
+    key = crypto.PKey()
+    key.generate_key(crypto.TYPE_RSA, 2048)
+    cert = crypto.X509()
+    cert.get_subject().CN = get_local_ip()
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(365*24*60*60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(key)
+    cert.sign(key, 'sha256')
+    
+    with open(cert_path, "wb") as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    with open(key_path, "wb") as f:
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+    
+    return str(cert_path), str(key_path)
 
 def cleanup_app_data(cert_file="server.crt", key_file="server.key"):
-    base_path = Path(__file__).parent.parent 
+    base_path = Path(__file__).parent.parent
     for f in [cert_file, key_file, ".spotify_cache"]:
         p = base_path / f
         if p.exists():
             try: p.unlink()
             except: pass
-
+    
     for root, dirs, files in os.walk(base_path):
         if "__pycache__" in dirs:
-            pycache_path = Path(root) / "__pycache__"
-            try: shutil.rmtree(pycache_path)
+            try: shutil.rmtree(Path(root) / "__pycache__")
             except: pass
